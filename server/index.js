5820df350b2278792828a7669a847d12d905e8d2
Marc <bernini.marc@gmail.com>
2022-10-29 17:47:11 +0200

    Add cors and body-parser package to app. Add user routes too

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
-const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const express = require("express");
+require("dotenv").config();
+const sequelize = require("./models");
+const user = require("./modules/user/user.routes");
+
 
 const port = 4242;
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
   // Resolving CORS problems by accepting * as origin
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
@@ -17,3 +26,6 @@ app.get('/hello', (req, res) => {
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
+
+
+app.use(user);
