b063d1ba8674f6688f5decf27ef60c2bb0764ba9
Marc <bernini.marc@gmail.com>
2022-10-30 17:20:47 +0100

    In create function, add hashing password and creating a user

--- server/modules/user/user.middlewares.js
+++ server/modules/user/user.middlewares.js
@@ -1,8 +1,15 @@
+const bcryptService = require("../../services/bcrypt");
+const userService = require("./user.services");
 
-
-exports.createUser = (req, res, next) => {
+exports.createUser = async (req, res, next) => {
     try {
         const {username, password} = req.body;
+        const hashPassword = await bcryptService.hash(password);
+        const user = {
+            username,
+            password: hashPassword
+        };
+        const newUser = await userService.create(user);
         return res.status(201);
     } catch (error) {
         next(error);
