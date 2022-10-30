d867c43dd7406df3f3865e49a6939a88d03a639b
Marc <bernini.marc@gmail.com>
2022-10-30 17:54:50 +0100

    feat: check if user exist. Check password if exist. Send error message if password is wrong. Create JWT and send it to front if it's ok

--- server/modules/user/user.middlewares.js
+++ server/modules/user/user.middlewares.js
@@ -1,16 +1,31 @@
 const bcryptService = require("../../services/bcrypt");
+const jwtService = require("../../services/jwt");
 const userService = require("./user.services");
 
 exports.createUser = async (req, res, next) => {
+    const { username, password } = req.body;
     try {
-        const {username, password} = req.body;
-        const hashPassword = await bcryptService.hash(password);
-        const user = {
-            username,
-            password: hashPassword
-        };
-        const newUser = await userService.create(user);
-        return res.status(201);
+        let passwordIsGood;
+        let newUser;
+        const existingUser = await userService.findByUsername(username);
+        if (!existingUser) {
+            const hashPassword = await bcryptService.hash(password);
+            const user = {
+                username,
+                password: hashPassword
+            };
+            newUser = await userService.create(user);
+        }
+        if (existingUser) {
+            passwordIsGood = await bcryptService.compare(password, existingUser.password);
+        }
+        if (!passwordIsGood && existingUser) {
+            return res.status(401).json({ message: "Mot de passe incorrect" });
+        }
+        const id = newUser?.id || existingUser.id;
+        const jwtPayload = { id };
+        const jwt = jwtService.sign(jwtPayload);
+        return res.status(201).json({ jwt });
     } catch (error) {
         next(error);
     }
