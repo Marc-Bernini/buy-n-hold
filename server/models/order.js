9c7ff30073225422967d6fba9e81102253d0662b
Marc <bernini.marc@gmail.com>
2022-10-30 20:30:18 +0100

    Add beforeValidation hooks to set 'expirationDate' before order's creation

--- server/models/order.js
+++ server/models/order.js
@@ -8,9 +8,28 @@ exports.defineOrder = (sequelize, DataTypes) => {
             allowNull: false,
             type: DataTypes.DATEONLY()
         }
+    }, {
+        hooks: {
+            beforeValidate: order => {
+                const expirationDate = defineExpirationDate(order);
+                console.log(expirationDate)
+                return order.expirationDate = new Date(expirationDate);
+            }
+        }
     });
 }
 
 exports.defineRelation = (Order, User) => {
     Order.belongsTo(User);
+}
+
+function defineExpirationDate(order) {
+    const fourteenDays = 14;
+    const fourteenDaysDate = new Date().getDate() + fourteenDays;
+    const dateInTwoWeeksTimeStamps = new Date().setDate(fourteenDaysDate);
+    const dateInTwoWeeksObject = new Date(dateInTwoWeeksTimeStamps)
+    const dateInTwoWeeks = dateInTwoWeeksObject.getDate();
+    const monthInTwoWeeks = dateInTwoWeeksObject.getMonth() + 1;
+    const yearInTwoWeeks = dateInTwoWeeksObject.getFullYear();
+    return `${yearInTwoWeeks}/${monthInTwoWeeks}/${dateInTwoWeeks}`;
 }
