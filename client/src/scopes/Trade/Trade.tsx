da44f35bdffd0ed5969f0ddc58e158c1e7a5e602
Marc <bernini.marc@gmail.com>
2022-10-30 20:32:40 +0100

    feat: add form to create order with validator and request to api.

--- client/src/scopes/Trade/Trade.tsx
+++ client/src/scopes/Trade/Trade.tsx
@@ -1,36 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
+import { Alert, Button, Col, Container, Form, InputGroup, Spinner, Row } from "react-bootstrap";
+import { useAppContext } from "../../contexts/AppContext";
+import { Order } from "../../interfaces/Order";
+import * as orderService from "../../services/order";
 
 import "./Trade.css";
 
 export default function Trade() {
+  const [validated, setValidated] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { token, setToken } = useAppContext();
+
+  const order: Order = {
+    price: null
+  };
+
+  const onSubmit = (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    event.stopPropagation();
+    setValidated(true);
+    if (!form.checkValidity()) {
+      return;
+    }
+    createOrder();
+  }
+
+  const createOrder = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const { data } = await orderService.createOrder(order, token);
+      setLoading(false);
+
+    } catch (error) {
+      const { response } = error;
+      let message: string;
+      if (response.status === 403) {
+        message = response.data;
+      } else {
+        message = "OUPS... Une erreur est survenue";
+      }
+      setError(message);
+      setLoading(false);
+    }
+  }
+
   return (
-    <div className="Trade">
-      <div className="table">
-        <div className="control">
-          <label htmlFor="price">
+    <Container className="bg-white py-5 my-auto">
+      {
+        error &&
+        <Col className="mx-auto" lg="3" xs="10">
+          <Alert variant="danger">
+            {error}
+          </Alert>
+        </Col>
+      }
+      <Form
+        noValidate
+        onSubmit={onSubmit}
+        validated={validated}
+      >
+        <Row className="align-items-center">
+          <Form.Label column="lg" lg={2} htmlFor="price">
             Price
-            <input type="number" id="price"></input>
-          </label>
-          <button>{`Ajouter une option d'achat`}</button>
+          </Form.Label>
+          <Col lg="2">
+            <Form.Control
+              id="price"
+              onChange={(e) => order.price = parseInt(e.target.value)}
+              placeholder="100"
+              required
+              type="number"
+            />
+          </Col>
+
+          <Col className="d-grid gap-2" lg="3">
+            <Button type="submit" variant="primary" disabled={loading}>
+              {
+                loading ?
+                  <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                  />
+                  :
+                  "Ajouter une option d'achat"
+              }
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+      <div className="Trade">
+        <div className="table">
+          <table>
+            <thead>
+              <tr>
+                <th>username</th>
+                <th>price</th>
+                <th>expiration</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Example</td>
+                <td>100$</td>
+                <td>{new Date().toDateString()}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <table>
-          <thead>
-            <tr>
-              <th>username</th>
-              <th>price</th>
-              <th>expiration</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Example</td>
-              <td>100$</td>
-              <td>{new Date().toDateString()}</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="stats"></div>
       </div>
-      <div className="stats"></div>
-    </div>
+    </Container>
+
   );
 }
