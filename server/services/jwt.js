174385a11f72504fbc8ea7a8334a9f9929deeb7c
Marc <bernini.marc@gmail.com>
2022-10-30 20:29:14 +0100

    feat: add function to verify JWT

--- server/services/jwt.js
+++ server/services/jwt.js
@@ -5,3 +5,8 @@ exports.sign = payload => {
     const algorithm = process.env.JWT_ALGORITHM;
     return jwt.sign(payload, privateKey, {algorithm});
 }
+
+exports.verify = token => {
+    const privateKey = process.env.JWT_PRIVATE_KEY;
+    return jwt.verify(token, privateKey);
+}
