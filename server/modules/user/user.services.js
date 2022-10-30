ddc467bc6e34d60a344cddb3594865389f98f116
Marc <bernini.marc@gmail.com>
2022-10-30 17:53:48 +0100

    feat: add function to find user with username

--- server/modules/user/user.services.js
+++ server/modules/user/user.services.js
@@ -3,3 +3,12 @@ const models = require("../../models");
 exports.create = user => {
     return models.User.create(user);
 }
+
+exports.findByUsername = username => {
+    return models.User.findOne({
+        attributes: ["id", "password"],
+        where: {
+            username
+        }
+    });
+}
