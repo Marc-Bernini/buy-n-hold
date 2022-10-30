f1f4ddb11875d19cb9215b10427f440aa134725a
Marc <bernini.marc@gmail.com>
2022-10-30 21:38:08 +0100

    feat: add function to get orders with user in API

--- client/src/services/order.ts
+++ client/src/services/order.ts
@@ -1,7 +1,10 @@
 import { Order } from "../interfaces/Order";
 import * as api from "./api";
 
-
 export const createOrder = (order: Order, token: string | null) => {
     return api.post(order, "order", token);
+}
+
+export const getOrders = (token: string | null) => {
+    return api.get("order", token);
 }
