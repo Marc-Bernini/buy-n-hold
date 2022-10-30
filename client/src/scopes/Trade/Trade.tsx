e8ab0b58fc0d20c41c079c533800511ed61611e4
Marc <bernini.marc@gmail.com>
2022-10-30 22:25:25 +0100

    feat: Using Orders component with props

--- client/src/scopes/Trade/Trade.tsx
+++ client/src/scopes/Trade/Trade.tsx
@@ -9,6 +9,7 @@ import {
   Spinner,
   Table
 } from "react-bootstrap";
+import Orders from "../../components/orders";
 import { useAppContext } from "../../contexts/AppContext";
 import { Order } from "../../interfaces/Order";
 import { User } from "../../interfaces/User";
@@ -125,87 +126,10 @@ export default function Trade() {
           </Col>
         </Row>
       </Form>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>Price</th>
-            <th>Expiration</th>
-            <th>Modifier</th>
-            <th>Supprimer</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders
-            .map((order, index) => (
-              <tr key={index}>
-                <td>{order.user.username}</td>
-                <td>
-                  {
-                    user?.id === order.user.id &&
-                    <Form.Control
-                      placeholder="price"
-                      aria-label="price"
-                      value={order.price}
-                      disabled={!onEdit || user?.id !== order.user.id}
-                    />
-                  }
-                  {
-                    user?.id !== order.user.id &&
-                    order.price
-                  }
-
-
-                </td>
-                <td>{order.expirationDate}</td>
-                <td>
-                  {
-                    !onEdit && user?.id === order.user.id &&
-                    <Button
-                      disabled={user?.id !== order.user.id}
-                      onClick={() => setOnEdit(!onEdit)}
-                      variant="primary"
-                    >
-                      Modifier
-                    </Button>
-                  }
-
-                  {
-                    onEdit && user?.id === order.user.id &&
-                    <>
-                      <Button
-                        className="mr-3"
-                        disabled={user?.id !== order.user.id}
-                        onClick={() => setOnEdit(!onEdit)}
-                        variant="success"
-                      >
-                        Valider
-                      </Button>
-                      <Button
-                        disabled={user?.id !== order.user.id}
-                        onClick={() => setOnEdit(false)}
-                        variant="warning"
-                      >
-                        Annuler
-                      </Button>
-                    </>
-                  }
-                </td>
-                <td>
-                  {
-                    user?.id === order.user.id &&
-                    <Button
-                      variant="danger"
-                      disabled={user?.id !== order.user.id}
-                    >
-                      Supprimer
-                    </Button>
-                  }
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </Table>
+      <Orders
+        orders={orders}
+        user={user}
+      ></Orders>
       <div className="stats"></div>
     </Container>
   );
