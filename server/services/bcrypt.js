eac69af61fed162d00d128b8d63391a3a7483306
Marc <bernini.marc@gmail.com>
2022-10-30 17:36:52 +0100

    feat: Add function for comparing password

--- server/services/bcrypt.js
+++ server/services/bcrypt.js
@@ -4,3 +4,5 @@ exports.hash = password => {
     const saltRounds = parseInt(process.env.SALT_ROUNDS);
     return bcrypt.hash(password, saltRounds);
 }
+
+exports.compare = (password, hashPassword) => bcrypt.compare(password, hashPassword);
